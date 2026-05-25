762de4e5dc72998e8d1b16fbaac26d2757e94096
Ramoskkj <m12776721@gmail.com>
2026-05-25 10:20:47 +0000

    v1

--- Sentinela.py
+++ Sentinela.py
@@ -1,1 +1,6 @@
-
+#--Variável Sentinla--
+while True:
+    comando = input("Digite o Coando, Se precisar para digite:Sair")
+    if comando == "sair":
+        break
+    print(F"Executando:{comando}")
