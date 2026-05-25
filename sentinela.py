5e6b21572444984b8fa7bd796a875dbc45c785a2
Ramoskkj <m12776721@gmail.com>
2026-05-25 07:13:56 -0300

    Delete sentinela.py

--- sentinela.py
+++
@@ -1,6 +0,0 @@
-#--Variável Sentinla--
-while True:
-    comando = input("Digite o Coando, Se precisar para digite:Sair")
-    if comando == "sair":
-        break
-    print(F"Executando:{comando}")
