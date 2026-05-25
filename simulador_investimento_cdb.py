85a2303c21540482c284715b44d3b398e98863bf
Ramoskkj <m12776721@gmail.com>
2026-05-25 23:17:31 +0000

    v1

--- simulador_investimento_cdb.py
+++ simulador_investimento_cdb.py
@@ -1,1 +1,26 @@
+#simulador_investimento_cdb
 
+dinheiro = float(input("Quanto você quer investir? R$ "))
+cdb = float(input("Quantos % do CDI é o CDB? "))
+
+cdi = 14.4
+
+# calcular rendimento
+taxa = (cdb * cdi) / 100
+
+# dividir por 12 meses
+taxa_mes = taxa / 12
+
+# 6 meses
+final = dinheiro
+
+for i in range(6):
+    final = final + (final * taxa_mes / 100)
+
+lucro = final - dinheiro
+
+print("\nResultado:")
+print("Valor inicial: R$", round(dinheiro, 2))
+print("Valor final: R$", round(final, 2))
+print("Lucro: R$", round(lucro, 2))
+MauricioRamos
