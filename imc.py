971619c6e63330b8885852ca942cc071ffdf55a3
Ramoskkj <m12776721@gmail.com>
2026-06-08 11:39:33 +0000

    v1

--- imc.py
+++ imc.py
@@ -1,1 +1,16 @@
+#Etapa1- Calculo do IMC
+def calculo_imc(imc):
+    imc = Peso/(altura*2)
+
+#Etapa2 - Teste de IMC
+def teste_imc(valor_imc)
+    if valor_imc >= 25
+    return "ACIMA DO PESO!"
+else:
+    return "PESO NORMAL"
+
+#Etapa3
+def resultado_imc(mensagem):
+    if mensagem == "ACIMA DO PESO"
+
 
