0c2b56b9e910a882102a420677a20d317bf25b86
Ramoskkj <m12776721@gmail.com>
2026-06-10 14:23:24 +0000

    v1

--- maior_palavra.py
+++ maior_palavra.py
@@ -1,1 +1,12 @@
-
+def maior_palavra.py
+    for palavra in ".,!?:":
+        texto = texto.replace(palavra, "")
+p = texto.split()
+if not p:
+    return " "
+maior = " "       
+for contador in P:
+    if len(contador) >= len(maior):
+        maior = contador
+return maior
+print(maior_palavra(Digite uma frase Aleatoria))        
