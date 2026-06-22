0b08e96e62f0d0f9212feebfa244229ef4d70aad
Ramoskkj <m12776721@gmail.com>
2026-06-22 11:30:26 +0000

    v1

--- painel_interativp.py
+++ painel_interativp.py
@@ -2,7 +2,7 @@ fila_espera=["senha 01", "senha 02," "senha 03", "senha 04"]
 senha_atual = 0
 while senha_atual < len(fila_espera):
     print("\n==========")
-    print(f"senha atual:{fila_espera[atual -1]}")
+    print(f"senha atual:{fila_espera[senha_atual: -1]}")
     if senha_atual > 0:
         print(f"senha anterior :{fila_espera[senha_atual -1]}")
     else:
@@ -10,7 +10,7 @@ while senha_atual < len(fila_espera):
 print("="*10)
 if senha_atual -1 < len(fila_espera):
     print(f"Próximo da fila:{fila_espera[senha_atual -1]}")
- else:
+else:
     print(f"fila vazia! Não ha mais senha.")   
 
 
